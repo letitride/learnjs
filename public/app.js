90b49d0fb675ab337c08539e3b46833241287794
letitride <ichikawa.fumiya@gmail.com>
2020-03-18 19:42:10 +0900

    3.2 データモデルを定義する

--- public/app.js
+++ public/app.js
@@ -1,9 +1,11 @@
 'use strict';
 var learnjs = {};
 
-learnjs.problemView = function(problemNumber){
+learnjs.problemView = function(data){
+  var problemNumber = parseInt(data, 10);
   var view = $('.templates .problem-view').clone();
-  view.find('.title').text('Problem #' + problemNumber + ' Coming soon!');
+  view.find('.title').text('Problem #' + problemNumber);
+  learnjs.applyObject(learnjs.problems[problemNumber - 1], view);
   return view;
 }
 
@@ -18,6 +20,21 @@ learnjs.showView = function(hash){
   }
 }
 
+learnjs.applyObject = function(obj, elem){
+  for(var key in obj){
+    elem.find('[data-name="' + key + '"]').text(obj[key]);
+  }
+}
+
+learnjs.problems = [
+  {
+    description: "What is truth?",
+    code: "function problem(){ return __; }"
+  },{
+    description: "Simple Math",
+    code: "function problem(){ return 42 === 6 * __; }"
+  }
+]
 
 learnjs.appOnReady = function(){
   window.onhashchange = function(){
